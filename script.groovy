c425673c38b880f069bd0a12c438651380b9bf45
Rashid-Khan-Github <rashidkhan7805@gmail.com>
2023-05-12 22:10:47 +0530

    repair script

--- script.groovy
+++ script.groovy
@@ -6,7 +6,7 @@ def buildJAR(){
 def buildImage(){
     echo "building the docker image.."
     withCredentials([usernamePassword(credentialsId: 'My-Docker-Hub', passwordVariable: 'PASS', usernameVariable: 'USER')]){
-        sh 'docker build -t dockerjackal/java-maven-app:JMA 2.0'
+        sh 'docker build -t dockerjackal/java-maven-app:JMA-2.0'
         sh "echo $PASS | docker login -u $USER --password-stdin"
         sh 'docker push dockerjackal/java-maven-app:JMA-2.0 .'
     }
